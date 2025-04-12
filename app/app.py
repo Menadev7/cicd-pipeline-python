91e7a9c30f533296703f85ca42d673ba73508810
Mena <rmena@sistecredito.com>
2025-04-12 01:34:31 -0500

    Fix bugs CI/CD

--- app/app.py
+++ app/app.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-  """Maneja la ruta principal '/' para mostrar un formulario y procesar operaciones matemáticas."""
     resultado = None
     if request.method == "POST":
         try:
