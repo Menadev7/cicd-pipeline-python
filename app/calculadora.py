bad8d82983b7636f71608f6dc20e8e60b58e3596
Mena <rmena@sistecredito.com>
2025-04-12 01:18:11 -0500

    Fix bugs CI/CD

--- app/calculadora.py
+++ app/calculadora.py
@@ -1,21 +1,19 @@
 """Este módulo calculadora"""
 # app/calculadora.py
-  """example"""
+
 def sumar(a, b):
     return a + b
 
 
-  """example"""
+
 def restar(a, b):
     return a - b
 
 
-  """example"""
 def multiplicar(a, b):
     return a * b
 
 
-  """example"""
 def dividir(a, b):
     if b == 0:
         raise ZeroDivisionError("No se puede dividir por cero")
